d7169f3b259e1e69983bf9932d22abb5ee3cdb21
Miraska <102532370+Miraska@users.noreply.github.com>
2024-11-01 01:08:49 +0300

    Update config.js

--- Translate-App/server/config/config.js
+++ Translate-App/server/config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    JWT_SECRET: "SECRET_KEY_RANDOM",
-    MONGO_URI: 'mongodb+srv://miraska007:Miras2005@cluster0.mnhu5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    JWT_SECRET: "",
+    MONGO_URI: '',
     SERVER_PORT: 3001
   };
